6f144f86d806f109c705d2cd105af7c8c51ba2e1
Levi <81602467+Levi14ram@users.noreply.github.com>
2021-06-18 18:48:16 -0500

    mejoras en index

--- javascriptjames/ingresartargeta.js
+++ javascriptjames/ingresartargeta.js
@@ -2,26 +2,27 @@ function config(){
  
 var numberTarget=document.getElementById("tG");
 var cvvTarget = document.getElementById("cV");
+val=0
 
 if (cvvTarget.value==""){
 
     alert("Por favor ingresar un numero de targeta ");
     numberTarget.focus();
-    return false;
+  
     
 
 }
 else if(cvvTarget.value==""){
     alert("Por favor ingrese su número de CVV ");
     cvvTarget.focus();
-    return false
+    
     
 }
-else if(cvvTarget.value=="" && cvvTarget.value=="" ){
-    document.location.reload()
-}
 
+ 
 }
+    
+
 
 
 
